f563a2e7f463f0f5dd45cab87145503baeeba54a
Isa Jafarov <isa.jafarov.01@gmail.com>
2024-07-10 00:23:05 +0400

    Update autoinstall.py

--- servers_setup/autoinstall.py
+++ servers_setup/autoinstall.py
@@ -127,6 +127,7 @@ if __name__ == '__main__':
     # kill the running webserver processes
     os.system("sudo pkill -9 nginx")
     os.system("sudo pkill -9 caddy")
+    os.system("sudo pkill -9 h2o")
     os.system("sudo /usr/local/lsws/bin/lswsctrl stop")
     
 
