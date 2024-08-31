7d7a3f55d81a7e98b75b372b331dcb3f51bdd763
Isa Jafarov <isa.jafarov.01@gmail.com>
2024-08-31 19:23:30 -0400

    synchronous quic implementation and building quic packets from scratch

--- evaluation/prett3_syn.py
+++ evaluation/prett3_syn.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+import aioquic.h3
+import aioquic.h3.connection
 import util
 import os
 import sys
@@ -11,13 +13,13 @@ from datetime import datetime
 from collections import deque
 from typing import BinaryIO, Callable, Deque, Dict, List, Optional, Union, cast
 from urllib.parse import urlparse
-
+import aioquic.buffer
 import asyncio
 import ssl
 import aioquic
 import wsproto
 import wsproto.events
-from aioquic.h3.connection import H3_ALPN, ErrorCode, H3Connection, FrameType
+from aioquic.h3.connection import H3_ALPN, ErrorCode, H3Connection, FrameType, StreamType, encode_frame, encode_settings
 from aioquic.h3.events import (
     DataReceived,
     H3Event,
@@ -37,12 +39,14 @@ import socket
 logger = logging.getLogger("client")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.settimeout(0.5)
+network_path = QuicNetworkPath('prett3.com')
 
 
 class HttpClient():
     def __init__(self, quic_conf, hostname) -> None:
         
         self.quic_conf = quic_conf
+        self.quic_conf.secrets_log_file = open('secrets.keylog', "a")
         self._quic = QuicConnection(configuration=self.quic_conf)
         self._http = H3Connection(self._quic)
         self.hostname = hostname
@@ -63,15 +67,15 @@ class HttpClient():
                 (b"user-agent", "PRETT3 client".encode()),
             ]
 
-        frame_data = self._http._encode_headers(stream_id, headers)
+        frame_data =  self._http._encode_headers(stream_id, headers)
 
-        #return frame_data
+        return aioquic.h3.connection.encode_frame(FrameType.HEADERS, frame_data)
 
-        self._http._quic.send_stream_data(
-            stream_id, aioquic.h3.connection.encode_frame(FrameType.HEADERS, frame_data), False)
+        #self._http._quic.send_stream_data(
+        #    stream_id, aioquic.h3.connection.encode_frame(FrameType.HEADERS, frame_data), False)
 
 
-    def get_builder(self):
+    def get_builder(self, epoch: Epoch):
     
         builder = QuicPacketBuilder(
             host_cid=self._http._quic.host_cid,
@@ -84,12 +88,16 @@ class HttpClient():
             peer_token=self._http._quic._peer_token,
             quic_logger=self._http._quic._quic_logger,
             spin_bit=self._http._quic._spin_bit,
-            
         )
     
-        epoch = Epoch.ONE_RTT # ZERO_RTT throws "Encryption key is not available" exception
+        #epoch = Epoch.ONE_RTT # ZERO_RTT throws "Encryption key is not available" exception
         crypto = self._http._quic._cryptos[epoch]
-        builder.start_packet(QuicPacketType.ONE_RTT, crypto)
+        
+        quic_packet_type = None
+        if epoch==Epoch.INITIAL: quic_packet_type = QuicPacketType.INITIAL
+        elif epoch==Epoch.HANDSHAKE: quic_packet_type = QuicPacketType.HANDSHAKE
+        elif epoch==Epoch.ONE_RTT: quic_packet_type = QuicPacketType.ONE_RTT
+        builder.start_packet(quic_packet_type, crypto)
 
         self._http._quic._packet_number += 1
 
@@ -97,20 +105,28 @@ class HttpClient():
 
     def send_quic_stream(self, frame_data):
 
-        builder = self.get_builder()
+        builder = self.get_builder(Epoch.ONE_RTT)
 
         buf = builder.start_frame(
-                QuicFrameType.STREAM_BASE,
-                capacity=1000,
+                QuicFrameType.STREAM_BASE | 2,
+                capacity=4, # not sure
                 #handler=stream.sender.on_data_delivery,
                 #handler_args=(frame.offset, frame.offset + len(frame.data), frame.fin),
             )
         buf.push_uint_var(0) # stream id
-        buf.push_uint_var(0) # offset
-        buf.push_uint16(100) # length
+        #buf.push_uint_var(0) # offset
+        '''
+        QUIC RFC 19.8
+        The OFF bit (0x04) in the frame type is set to indicate that there is an Offset field present. 
+        When set to 1, the Offset field is present. 
+        When set to 0, the Offset field is absent and the Stream Data starts at an offset of 0 
+        (that is, the frame contains the first bytes of the stream, or the end of a stream that includes no data).
+        '''
+        buf.push_uint16(len(frame_data) | 0x4000) # length
         buf.push_bytes(frame_data) # data
 
         self.send_quic_frames_from_builder(builder)
+
 
     def send_quic_ack(self, acked_packet_num):
         
@@ -131,46 +147,190 @@ class HttpClient():
 
 
     def send_quic_frames_from_builder(self, builder:QuicPacketBuilder):
+        print(">>> send_quic_frames_from_builder: start")
         datagrams, packets = builder.flush()
 
         for data in datagrams:
+            print("\nSending message:")
+            print(data)
             sock.sendto(data, (self.hostname, 443))
     
 
     def connect(self):
-        self._quic.connect(self.hostname, time.time())
+        self._quic.connect(self.hostname, time.process_time())
         print("\n Transmitting:\n")
 
-        for data, addr in self._quic.datagrams_to_send(now=time.time()):
+        for data, addr in self._quic.datagrams_to_send(now=time.process_time()):
+            print("\nSending message:")
             print(data)
             sock.sendto(data, (self.hostname, 443))
-            
 
+        
+    def complete_connection(self):
+        print("\n>>> complete_connection: start")
+        #self._quic._discard_epoch(tls.Epoch.INITIAL) # datagrams_to_send, if sent_handshake is True
+ 
+        #self._quic._update_traffic_key(tls.Direction.ENCRYPT, Epoch.HANDSHAKE, tls.CipherSuite.AES_256_GCM_SHA384, ''.encode())
+        
+        builder = self.get_builder(Epoch.HANDSHAKE)
+        self._quic._write_handshake(builder, Epoch.HANDSHAKE, time.process_time())
+        self.send_quic_frames_from_builder(builder)
     
+
+        """
+        How aioquic creates/sends streams:
+        1. _quic.send_stream_data
+            - calls _quic._get_or_create_stream_for_send(), which creates the stream and appends to _quic._streams_queue
+            - writes data to that stream.sender
+        2. _quic.datagrams_to_send() passes builder to _quic._write_application(), which
+            - gets the stream from _quic._streams_queue 
+            - calls _quic._write_stream_frame to create the frame
+        """
     
+    ''' for test purposes
+    def open_qpack_streamsq(self):
+        print(">>> open_qpack_streams: start")
+        builder = self.get_builder(Epoch.ONE_RTT)
+        self._quic._write_application(builder, network_path, time.process_time())    
+        self.send_quic_frames_from_builder(builder)
+    '''
+
+    def open_qpack_streams(self):
+        """
+        1. Crafting
+        _http._init_connection()
+            - gonna create 3 uni streams via _http._create_uni_stream()
+        
+        _http._create_uni_stream()
+            - gonna create QUIC stream by calling _quic.send_stream_data() with stream id and encoded _http.StreamType
+
+        _quic.send_stream_data()
+            - gonna create the stream by calling _quic._get_or_create_stream_for_send()
+            - appends to _quic._streams_queue list
+            - writes data to that stream.sender
+        
+        _quic._get_or_create_stream_for_send()
+            - creates QuicStream
+        
+            
+        2. Sending
+        _quic.datagrams_to_send() 
+            - passes builder to _quic._write_application()
+
+        _quic._write_application()
+            - gets the stream from _quic._streams_queue list
+            - gonna create the stream frame by passing builder to _quic._write_stream_frame
+
+        _quic._write_stream_frame()
+            - creates frame by calling stream.sender.get_frame()
+
+        stream.sender.get_frame()
+            - creates QuicStreamFrame
+        """
+        
+        print(">>> open_qpack_streams: start")
+
+        settings={
+            aioquic.h3.connection.Setting.QPACK_MAX_TABLE_CAPACITY: 2323,# self._http._max_table_capacity,
+            aioquic.h3.connection.Setting.QPACK_BLOCKED_STREAMS: self._http._blocked_streams,
+            aioquic.h3.connection.Setting.ENABLE_CONNECT_PROTOCOL: 1,
+            aioquic.h3.connection.Setting.DUMMY: 1
+        }
+        encoded_settings_frame = encode_frame(FrameType.SETTINGS, encode_settings(settings))
+        stream2_frame = QuicStreamFrame(
+            data=
+             bytes( aioquic.buffer.encode_uint_var(StreamType.CONTROL) + encoded_settings_frame), #aioquic.buffer.encode_uint_var(StreamType.CONTROL),
+            offset=0,
+            fin=False
+        )
+       
+        
+        stream6_frame = QuicStreamFrame(
+            data=aioquic.buffer.encode_uint_var(StreamType.QPACK_ENCODER),
+            offset=0,
+            fin=False
+        )
+
+        stream10_frame = QuicStreamFrame(
+            data=aioquic.buffer.encode_uint_var(StreamType.QPACK_DECODER),
+            offset=0,
+            fin=False
+        )
+
+
+        builder = self.get_builder(Epoch.ONE_RTT)
+
+
+        # Frame 1
+        buf1 = builder.start_frame(
+                QuicFrameType.STREAM_BASE | 2, 
+                capacity=4, #checked
+            )
+        buf1.push_uint_var(2) # stream id
+        #buf1.push_uint_var(0) # offset. IMPORTANT!!! _QUIC._write_stream_frame() does not set offset for these frames.
+        buf1.push_uint16( len(stream2_frame.data) | 0x4000 ) #(16399) #(len(stream2_frame.data) | 0x4000) # length
+        buf1.push_bytes(stream2_frame.data) # data
+        
+        
+        # Frame 2
+        buf2 = builder.start_frame(
+                QuicFrameType.STREAM_BASE | 2,
+                capacity=4, #checked
+            )
+        buf2.push_uint_var(6)
+        #buf2.push_uint_var(0) # offset
+        buf2.push_uint16( len(stream6_frame.data) | 0x4000 )  #(16385) #(len(stream6_frame.data) | 0x4000)
+        buf2.push_bytes(stream6_frame.data)
+
+
+        # Frame 3
+        buf3 = builder.start_frame(
+                QuicFrameType.STREAM_BASE | 2,
+                capacity=4, #checked
+            )
+        buf3.push_uint_var(10)
+        #buf3.push_uint_var(0) # offset
+        buf3.push_uint16(len(stream10_frame.data) | 0x4000) #(16385) | 0x4000)
+        buf3.push_bytes(stream10_frame.data)        
+
+        self.send_quic_frames_from_builder(builder)
+
+
+    def read_from_buffer(self):
+        # receive server's response
+        try:
+            while True:
+                data, addr = sock.recvfrom(2048) # 1024 causes problems
+                print("\nReceived message:\n%s" % data)
+                self._quic.receive_datagram(data, addr=self.hostname, now=time.process_time())
+        except socket.timeout: pass
 
 
 def main(
     configuration: QuicConfiguration,
     url: str
 ) -> None:
-    
+
     h3client = HttpClient(configuration, urlparse(url).netloc)
 
-    # connection initialization
+
     h3client.connect()
+    h3client.read_from_buffer()
+    
+    time.sleep(0.1)
 
-    # receive server's response
-    try:
-        while True:
-            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-            print("\nReceived message:\n%s" % data)
-    except socket.timeout: pass
+    h3client.complete_connection()
+    h3client.read_from_buffer()
 
+    time.sleep(0.1)
+    h3client.open_qpack_streams()
+    h3client.read_from_buffer()
 
-    # try to send a simple quic frame
-    h3client.send_quic_ack(10)
-
+    time.sleep(0.1)
+    headers_data = h3client.craft_sample_headers_frame()
+    h3client.send_quic_stream(headers_data)
+    h3client.read_from_buffer()
+    
 
 
 def init(args):
@@ -289,11 +449,14 @@ if __name__ == "__main__":
     """ aioquic's code till here """
 
     ### General setting ###
-    init(args)
+    #init(args)
     
     ### Extract initial state machine ###
-    http3_basic_messages = util.h3msg_from_pcap(args.pcap, client_only=True)
-
+    #http3_basic_messages = util.h3msg_from_pcap(args.pcap, client_only=True)
+    print( QuicFrameType.STREAM_BASE | 2 )
+    print( aioquic.buffer.encode_uint_var(StreamType.CONTROL) )
+    print( aioquic.buffer.encode_uint_var(StreamType.QPACK_ENCODER) )
+    print( aioquic.buffer.encode_uint_var(StreamType.QPACK_DECODER) )
     main(
             configuration=configuration,
             url=args.url
